33240e62dc745ed677ed9e3cdba7915003504b1e
patrick <patrick@patrick-LIFEBOOK-E782>
2018-06-06 22:42:00 +0200

    Testweise Skript zum Teil auskommentiert

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -6,8 +6,8 @@ docker build -t parcelsize .
 # Deploy the docker image to the resource server
 docker save --output parcelsize.img parcelsize:latest
 
-scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsize.img $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsize
+#scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsize.img $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsize
 rm parcelsize.img #directly delete the parcelsize image file again
 
 # Deploy the ui component to the resource server
-scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/parcel-size.component.js
+#scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/parcel-size.component.js
