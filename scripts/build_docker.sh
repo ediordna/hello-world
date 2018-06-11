71c9ff2973e5e6c9762a18993bb2577d35c5ab7d
patrick <patrick@patrick-LIFEBOOK-E782>
2018-06-11 14:20:53 +0200

    Skript ausführbar gemacht

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Build the docker image for the parcel size service
-docker build -t parcelsizecomponent .
+docker build -t parcelsize .
 
 # Deploy the docker image to the resource server
-docker save --output parcelsize.img parcelsizecomponent:latest
+docker save --output parcelsize.img parcelsize:latest
 
-#scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsize.img $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsize
-#rm parcelsize.img #Directly delete the parcelsize image file again
+scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsize.img $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsize
+rm parcelsize.img #Directly delete the parcelsize image file again
 
-# Deploy the ui component to the resource server
-#scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/parcel-size.component.js
+# Deploy the ui component to the resource server 
+scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/parcel-size.component.js
