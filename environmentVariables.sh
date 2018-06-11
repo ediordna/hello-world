51dbec9749fb81d25eeddd07000e99d931396076
patrick <patrick@patrick-LIFEBOOK-E782>
2018-06-11 16:42:16 +0200

    temp environmentVar

---
+++ environmentVariables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export RESOURCE_SERVER="192.168.50.100"
+export RESOURCE_SERVER_USER="vagrant"
+export RESOURCE_SERVER_ROOT="/home/user/resources"
+export RESOURCE_SERVER_KEY="/home/patrick/AssetServerVM/ssh_key"
+
+
+./scripts/build_docker.sh
