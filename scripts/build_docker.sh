20b7283554e624c752c75bde478c6fadfc86101e
patrick <patrick@patrick-LIFEBOOK-E782>
2018-06-15 17:48:13 +0200

    renamed image

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -5,12 +5,12 @@ docker build -t parcelsize .
 echo Successfully built docker image
 
 # Deploy the docker image to the resource server
-docker save --output parcelsize.img parcelsize:latest 
-echo Sucessfully created parcelsize.img
+docker save --output parcelsizecomponent.tar parcelsize:latest 
+echo Sucessfully created parcelsizecomponent.tar
 
-scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsize.img $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsize.tar
+scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsizecomponent.tar $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsizecomponent.tar
 #rm parcelsize.img #Directly delete the parcelsize image file again
-echo Successfully deployed parcelsize.img to asset server
+echo Successfully deployed parcelsizecomponent.tar to asset server
 
 # Deploy the ui component to the resource server 
 scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/parcel-size.component.js
