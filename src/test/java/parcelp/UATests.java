4e3d4a1ce70aaeb2beff6dbc0316690e34df40fe
patrick <patrick@patrick-LIFEBOOK-E782>
2018-06-25 18:44:17 +0200

    updated id of button

--- src/test/java/parcelp/UATests.java
+++ src/test/java/parcelp/UATests.java
@@ -33,7 +33,7 @@ public class UATests {
 		driver.findElement(By.id("cfg-ps-width")).sendKeys("10");
 		driver.findElement(By.id("cfg-rs-height")).clear();
 		driver.findElement(By.id("cfg-rs-height")).sendKeys("10");
-		driver.findElement(By.xpath("//div[@id='btn-ps-calc-size']/h3")).click();
+		driver.findElement(By.id("btn-ps-calc-size")).click();
 		Thread.sleep(5000);
 		assertEquals("Paketgröße: S", driver.findElement(By.id("ps-result-size")).getText());
 	}
