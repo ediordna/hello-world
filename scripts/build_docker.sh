f9e3b4af6f97adc1583a9d516f93f1fb35dce2a9
patrick <patrick@patrick-LIFEBOOK-E782>
2018-06-25 18:50:46 +0200

    added ConnectTimeout parameter for faster Timeout

--- scripts/build_docker.sh
+++ scripts/build_docker.sh
@@ -8,10 +8,10 @@ echo Successfully built docker image
 docker save --output parcelsizecomponent.tar parcelsizecomponent:latest 
 echo Sucessfully created parcelsizecomponent.tar
 
-scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsizecomponent.tar $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsizecomponent.tar
+scp -o ConnectTimeout=5 -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY parcelsizecomponent.tar $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/images/parcelsizecomponent.tar
 #rm parcelsize.img #Directly delete the parcelsize image file again
 echo Successfully deployed parcelsizecomponent.tar to asset server
 
 # Deploy the ui component to the resource server 
-scp -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/ui-comp.js
+scp -o ConnectTimeout=5 -o StrictHostKeyChecking=no -i $RESOURCE_SERVER_KEY ui/ui-comp.js $RESOURCE_SERVER_USER@$RESOURCE_SERVER:$RESOURCE_SERVER_ROOT/ui/ui-comp.js
 echo Successfully deployed ui-comp.js to asset server
